2980b2051718ade5e272afc81761d0dea5f5caa4
Marcin <chojnacki8@gmail.com>
2019-04-15 17:46:41 +0200

    Add styling to index

--- src/main/java/com/chojnacki/shoppinglist/configuration/SecurityConfiguration.java
+++ src/main/java/com/chojnacki/shoppinglist/configuration/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                     .loginPage("/login")
+                    .defaultSuccessUrl("/", true)
                     .permitAll()
                 .and()
                     .logout()
